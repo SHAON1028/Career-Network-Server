fcb64010e39b54f9842cbd06f6effa0f18314cc3
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-01-16 16:52:17 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -11,10 +12,31 @@ app.use(express.json());
 
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`Server is running on ${port}`)
 });
