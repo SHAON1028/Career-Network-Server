967923503545eb434076ea32781d8aff52e22833
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-01-18 19:21:26 +0600

    usercallection

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const userCollection = client.db('carrernetwork').collection('users');
+
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 
 
