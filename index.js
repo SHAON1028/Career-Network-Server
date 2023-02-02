b1d130a8b2af9ce286d976029655a4082fad94f3
shaon28 <67582213+SHAON1028@users.noreply.github.com>
2023-02-02 23:19:31 +0600

    job details

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -97,7 +98,16 @@ async function run() {
             const count = await jobsCollecton.estimatedDocumentCount()
             res.send({ count, jobs })
         })
+        // job details
+        app.get('/alljobs/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = {_id:ObjectId(id)}
 
+            const job = await jobsCollecton.findOne(query)
+            res.send(job)
+        })     
+  
 
     }
     finally {
